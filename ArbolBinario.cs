d5bc51488f1d6328ad1eb7ea488bf100bd2f7549
Anton Olguin <antonolguin1@gmail.com>
2025-03-19 21:25:53 -0600

    Echen paja 🗣🔥🔥

--- ArbolBinario.cs
+++ ArbolBinario.cs
@@ -3,10 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace ArbolesChepeEchenPaja______
 {
     class ArbolBinario
     {
+        private Nodo root;
+
+        public ArbolBinario()
+        {
+            root = new Nodo();
+        }
+
+        public void Add(int data)
+        {
+            if (root.Data == null)
+            {
+                root.Data = data;
+                return;
+            }
+
+            Nodo path = root;
+            while (path != null)
+            {
+                if (data > root.Data)
+                {
+                    path = path.Right;
+                }
+
+                else if (data < root.Data)
+                {
+                    path = path.Left;
+                }
+            }
+
+            path = new Nodo(data);
+            Console.WriteLine(data);
+        }
+
+        public void PreOrder()
+        {
+
+        }
     }
 }
--- Nodo.cs
+++ Nodo.cs
@@ -3,10 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace ArbolesChepeEchenPaja______
 {
     class Nodo
     {
+        private int? data;
+        private Nodo left;
+        private Nodo right;
+
+        public Nodo(int data, Nodo left, Nodo right)
+        {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+
+        public Nodo(int data)
+        {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+
+        public Nodo()
+        {
+            data = null;
+            left = null;
+            right = null;
+        }
+
+        public int? Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+
+        public Nodo Left
+        {
+            get { return left; }
+            set { left = value; }
+        }
+
+        public Nodo Right
+        {
+            get { return right; }
+            set { right = value; }
+        }
     }
 }
